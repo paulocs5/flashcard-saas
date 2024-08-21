f470fe9ffecf02f26344503581c5d4044b57ebe0
paulocs5 <paulocriollo617@gmail.com>
2024-08-20 20:27:53 -0400

    updated files

---
+++ app/sign-up/[[...sign-up]]/page.js
@@ -0,0 +1,46 @@
+'use client'
+import { SignIn } from "@clerk/clerk-react";
+import { SignUp } from "@clerk/nextjs";
+import { AppBar, Button, Container, Toolbar, Typography, Box} from "@mui/material";
+import Link from "next/link";
+
+
+export default function SignUpPage(){
+      return < Container maxWidth="100vw">
+            <AppBar position="static" sx={{backgroundColor: "#3f52b5"}}> 
+                  <Toolbar> 
+                        <Typography 
+                              variant="h6"
+                              sx={{
+                              flexGrow: 1,
+                              }}
+                        > 
+                              FlashyCard
+                        </Typography>
+                        <Button color="inherit"> 
+                              <Link href="/sign-in" passHref>
+                                    Login
+                              </Link>
+                        </Button>
+                        <Button color="inherit"> 
+                              <Link href="/sign-up" passHref>
+                                    Sign Up
+                              </Link>
+                        </Button>
+                  </Toolbar>
+            </AppBar>
+            <Box
+                  display="flex"
+                  flexDirection="column"
+                  alignItems="center" 
+                  justifyContent="center"  
+            > 
+                  <Typography variant="h4"> Sign Up </Typography>
+                  <SignUp/>
+            </Box>
+
+
+      </Container>
+
+}
+
